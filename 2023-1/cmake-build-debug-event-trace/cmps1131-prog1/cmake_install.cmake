924218882dbddc7cdb4485b8aeeea7e7f2a9a7b3
Jennessa Sierra <143227481+jennxsierra@users.noreply.github.com>
2024-01-23 13:13:04 -0600

    misc

---
+++ 2023-1/cmake-build-debug-event-trace/cmps1131-prog1/cmake_install.cmake
@@ -0,0 +1,109 @@
+# Install script for directory: C:/Users/jennx/Documents/GitHub/ub-bint/2023-1/cmps1131-prog1
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/2023-1")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2023.2.1/bin/mingw/bin/objdump.exe")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/jennx/Documents/GitHub/ub-bint/2023-1/cmake-build-debug-event-trace/cmps1131-prog1/week1/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/jennx/Documents/GitHub/ub-bint/2023-1/cmake-build-debug-event-trace/cmps1131-prog1/week2/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/jennx/Documents/GitHub/ub-bint/2023-1/cmake-build-debug-event-trace/cmps1131-prog1/week3/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/jennx/Documents/GitHub/ub-bint/2023-1/cmake-build-debug-event-trace/cmps1131-prog1/week4/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/jennx/Documents/GitHub/ub-bint/2023-1/cmake-build-debug-event-trace/cmps1131-prog1/week5/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/jennx/Documents/GitHub/ub-bint/2023-1/cmake-build-debug-event-trace/cmps1131-prog1/week6/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/jennx/Documents/GitHub/ub-bint/2023-1/cmake-build-debug-event-trace/cmps1131-prog1/week7/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/jennx/Documents/GitHub/ub-bint/2023-1/cmake-build-debug-event-trace/cmps1131-prog1/week8/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/jennx/Documents/GitHub/ub-bint/2023-1/cmake-build-debug-event-trace/cmps1131-prog1/week9/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/jennx/Documents/GitHub/ub-bint/2023-1/cmake-build-debug-event-trace/cmps1131-prog1/week10/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/jennx/Documents/GitHub/ub-bint/2023-1/cmake-build-debug-event-trace/cmps1131-prog1/week11/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/jennx/Documents/GitHub/ub-bint/2023-1/cmake-build-debug-event-trace/cmps1131-prog1/week12/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/jennx/Documents/GitHub/ub-bint/2023-1/cmake-build-debug-event-trace/cmps1131-prog1/week13/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/jennx/Documents/GitHub/ub-bint/2023-1/cmake-build-debug-event-trace/cmps1131-prog1/week15/cmake_install.cmake")
+endif()
+
